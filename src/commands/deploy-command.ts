4765371496904b1db67f0facf0d523c7688d1844
Xavier Monin <xaviermonin@hotmail.com>
2022-11-02 00:06:49 +0100

    Deploy in debug mode by default

--- src/commands/deploy-command.ts
+++ src/commands/deploy-command.ts
@@ -2,16 +2,21 @@ import { Command } from 'commander';
 import { EntryPointBuildMode } from '../core/entry-point';
 import { Workspace } from '../core/workspace';
 
-async function deployAction() {
+interface Options {
+    production: boolean;
+}
+
+async function deployAction(options: Options) {
     if (!Workspace.isWorkspace(".")) {
         console.error("This action must be run in a primno project directory");
         return;
     }
 
     const currentWs = new Workspace(".");
-    await currentWs.deploy({ production: true, mode: EntryPointBuildMode.primnoEmbedded });
+    await currentWs.deploy({ production: options.production, mode: EntryPointBuildMode.primnoEmbedded });
 };
 
 export const deployCommand = new Command('deploy')
-    .description('deploy workspace in production mode')
+    .description('deploy workspace')
+    .option("-p, --production", "production mode", false)
     .action(deployAction);
