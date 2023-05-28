16ea58ca76debf05d14d344f0685a1610accf9a6
Xavier Monin <xaviermonin@hotmail.com>
2023-05-28 14:19:43 +0200

    chore: switch to mono repo

--- packages/cli/jest.config.ts
+++ packages/cli/jest.config.ts
@@ -2,10 +2,7 @@ import type { JestConfigWithTsJest } from 'ts-jest'
 
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  projects: [
-    "./packages/*"
-  ]
+  testEnvironment: 'node'
 };
 
 export default config;
